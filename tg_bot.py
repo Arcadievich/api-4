cb77e200b15f0713e47cba6a210ae44d8f80a5db
Arcadievich <user_274@mail.ru>
2025-09-24 19:32:32 +0300

    Publish photo to Telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -6,9 +6,13 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     bot_token = os.getenv('TG_BOT_TOKEN')
+    tg_channel_id = os.getenv('TG_CHANNEL_ID')
 
     bot = telegram.Bot(token=bot_token)
-    bot.send_message(text='Hi, all!', chat_id='@B_Space_P')
+    bot.send_document(
+        chat_id=tg_channel_id,
+        document=open('images/spacex6.jpg', 'rb')
+        )
 
 
 if __name__ == '__main__':
